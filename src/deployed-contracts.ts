c7a53f9e13a18bfcd604562040c0744a522be040
Philip Mataras <vilenarios@gmail.com>
2022-06-23 01:10:34 -0400

    Updated latest contracts

--- src/deployed-contracts.ts
+++ src/deployed-contracts.ts
@@ -1,10 +1,10 @@
 // ~~ Inidicate contract id ~~
 export const deployedContracts = {
-  contractTxId: "Jq2EQOgBiCELHJvTlYzZ0C1PD0wILyWV2SkU3QuSKUo",
-  sourceTxId: "7hL0La2KMapdJI6yIGnb4f4IjvhlGQyXnqpWc0i0d_w",
+  contractTxId: "ACpV_CzOfNxh8LhPM-FMcsLNpVYcy-KyTzEQ1UD5avg",
+  sourceTxId: "JIIB01pRbNK2-UyNxwQK-6eknrjENMTpTvQmB8ZDzQg",
 };
 
 export const deployedTestContracts = {
-  contractTxId: "Agc9UmnwOvMlPOqTJfbDhvAiga08X9YXNRY0qEQTrGA",
-  sourceTxId: "W4pB-xhcZ2IzwXiWmKGQuYmd5cuKBozGAeY-ZRMh0VU",
+  contractTxId: "-HxRqdZSSK9P5-jQ5Agkv6Dcw5JZ37xJE24eUn2GBZI",
+  sourceTxId: "8xv_lw8DydXAHaGjJhJrRRAODmb0eiaPk3wi_HHxxhE",
 };
