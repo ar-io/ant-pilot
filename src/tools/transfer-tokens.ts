e258541dc39f1ab679dceb9129ef77e656c57107
vilenarios <vilenarios@gmail.com>
2022-06-29 10:17:55 -0400

    Adds script for transfer Test ArNS Tokens

---
+++ src/tools/transfer-tokens.ts
@@ -0,0 +1,53 @@
+import Arweave from "arweave";
+import { LoggerFactory, WarpNodeFactory } from "warp-contracts";
+import * as fs from "fs";
+import { JWKInterface } from "arweave/node/lib/wallet";
+import { keyfile } from "../constants";
+
+(async () => {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~UPDATE THE BELOW~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // The recipient target of the token transfer
+  const target = "JcC4ZLUY76vmWha5y6RwKsFqYTrMZhbockl8iM9p5lQ";
+
+  // The amount of tokens to be transferred
+  const qty = 1_000_000_000;
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  // This is the production ArNS Registry Smartweave Contract
+  const arnsRegistryContractTxId = "bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U";
+
+  // Initialize Arweave
+  const arweave = Arweave.init({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
+  });
+
+  // Initialize `LoggerFactory`
+  LoggerFactory.INST.logLevel("error");
+
+  // Initialize SmartWeave
+  const smartweave = WarpNodeFactory.memCached(arweave);
+
+  // Get the key file used for the distribution
+  const wallet: JWKInterface = JSON.parse(
+    await fs.readFileSync(keyfile).toString()
+  );
+  const walletAddress = await arweave.wallets.jwkToAddress(wallet);
+
+  // Read the ANT Registry Contract
+  console.log(
+    "Transfering %s tokens from %s to %s",
+    qty,
+    walletAddress,
+    target
+  );
+  const pst = smartweave.pst(arnsRegistryContractTxId);
+  pst.connect(wallet);
+  await pst.transfer({
+    target,
+    qty,
+  });
+
+  console.log("Finished transferring tokens");
+})();
