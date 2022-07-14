82fe2fa3bb5cb9850c2f2ccda05b0a94c9edbc11
vilenarios <vilenarios@gmail.com>
2022-07-13 20:51:28 -0400

    Adds support for controller

--- src/contracts/types/types.ts
+++ src/contracts/types/types.ts
@@ -3,11 +3,12 @@ export interface ANTState {
   ticker: string; // A short token symbol, shown in block explorers and marketplaces
   name: string;   // The friendly name of the token, shown in block explorers and marketplaces
   owner: string;  // The owner of this contract who can execute specific methods
+  controller: string // The controller of the records, who can add/change subdomains and their settings
   records: {     // A list of all subdomains and their corresponding Arweave Transaction IDs and TTLs
-    [subDomain: string]: [{ // the subdomain used, default is the root @
+    [subDomain: string]: { // the subdomain used, default is the root @
       transactionId: string, // The transaction ID that the subdomain points to.
       ttlSeconds: number, // The amount of time (in seconds) this transaction is cached for, default at 900 seconds
-    }]
+    }
   };
   balances: { // A list of all outstanding, positive, token balances
     [address: string]: number;
@@ -48,6 +49,7 @@ export type PstFunction =
   | "setRecord"
   | "setName"
   | "setTicker"
+  | "setController"
   | "removeRecord"
   | "balance";
 
