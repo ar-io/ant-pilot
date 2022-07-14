658a34bf7d4cd1d03cbe8bcd87a13386fed6af89
Philip Mataras <vilenarios@gmail.com>
2022-07-13 22:06:23 -0400

    fixes comma and semicolon

--- src/contracts/types/types.ts
+++ src/contracts/types/types.ts
@@ -3,11 +3,11 @@ export interface ANTState {
   ticker: string; // A short token symbol, shown in block explorers and marketplaces
   name: string;   // The friendly name of the token, shown in block explorers and marketplaces
   owner: string;  // The owner of this contract who can execute specific methods
-  controller: string // The controller of the records, who can add/change subdomains and their settings
+  controller: string; // The controller of the records, who can add/change subdomains and their settings
   records: {     // A list of all subdomains and their corresponding Arweave Transaction IDs and TTLs
     [subDomain: string]: { // the subdomain used, default is the root @
       transactionId: string, // The transaction ID that the subdomain points to.
-      ttlSeconds: number, // The amount of time (in seconds) this transaction is cached for, default at 900 seconds
+      ttlSeconds: number // The amount of time (in seconds) this transaction is cached for, default at 900 seconds
     }
   };
   balances: { // A list of all outstanding, positive, token balances
