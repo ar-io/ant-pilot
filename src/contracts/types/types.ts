5ab8fcb8ea6abb4f39f7f95fadea77eb425f5423
vilenarios <vilenarios@gmail.com>
2022-07-18 19:18:21 -0400

    Fixes incorrect type on records

--- src/contracts/types/types.ts
+++ src/contracts/types/types.ts
@@ -1,15 +1,18 @@
 // ~~ Write types for your contract ~~
 export interface ANTState {
   ticker: string; // A short token symbol, shown in block explorers and marketplaces
-  name: string;   // The friendly name of the token, shown in block explorers and marketplaces
-  owner: string;  // The owner of this contract who can execute specific methods
-  records: {     // A list of all subdomains and their corresponding Arweave Transaction IDs and TTLs
-    [subDomain: string]: [{ // the subdomain used, default is the root @
-      transactionId: string, // The transaction ID that the subdomain points to.
-      ttlSeconds: number, // The amount of time (in seconds) this transaction is cached for, default at 900 seconds
-    }]
+  name: string; // The friendly name of the token, shown in block explorers and marketplaces
+  owner: string; // The owner of this contract who can execute specific methods
+  records: {
+    // A list of all subdomains and their corresponding Arweave Transaction IDs and TTLs
+    [subDomain: string]: {
+      // the subdomain used, default is the root @
+      transactionId: string; // The transaction ID that the subdomain points to.
+      ttlSeconds: number; // The amount of time (in seconds) this transaction is cached for, default at 900 seconds
+    };
   };
-  balances: { // A list of all outstanding, positive, token balances
+  balances: {
+    // A list of all outstanding, positive, token balances
     [address: string]: number;
   };
 }
@@ -51,4 +54,7 @@ export type PstFunction =
   | "removeRecord"
   | "balance";
 
-export type ContractResult = { state: ANTState } | { result: PstResult } | { result: ANTSubDomainResult };
+export type ContractResult =
+  | { state: ANTState }
+  | { result: PstResult }
+  | { result: ANTSubDomainResult };
