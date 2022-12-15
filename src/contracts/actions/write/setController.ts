1c26981f572103bd99815a95b05b7ea865ab0b5b
vilenarios <vilenarios@gmail.com>
2022-12-15 14:09:17 -0500

    Removes unnecessary balance check

--- src/contracts/actions/write/setController.ts
+++ src/contracts/actions/write/setController.ts
@@ -7,17 +7,12 @@ export const setController = async (
   { caller, input: { target } }: PstAction
 ): Promise<ContractResult> => {
   const owner = state.owner;
-  const balances = state.balances;
   if (!target) {
     throw new ContractError("No target specified");
   }
 
   if (caller !== owner) {
     throw new ContractError(`Caller is not the token owner!`);
-  }
-
-  if (balances[caller] < 1) {
-    throw new ContractError(`Caller does not have a token balance!`);
   }
 
   state.controller = target;
