0c7401085b0f696f653a19586ded2065baea5d6d
vilenarios <vilenarios@gmail.com>
2023-10-04 11:54:57 -0400

    Cleans up constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-export const url = {
-  redstoneGateway: "",
-};
-
 export const MIN_TTL_LENGTH = 900; // 15 minutes
 export const MAX_TTL_LENGTH = 2_592_000;  // 30 days
 export const MAX_NAME_LENGTH = 20;
@@ -9,11 +5,4 @@ export const TX_ID_LENGTH = 43;
 export const ARWEAVE_ID_REGEX = new RegExp("^[a-zA-Z0-9_-]{43}$");
 export const UNDERNAME_REGEX = new RegExp("^[a-zA-Z0-9_-]+$");
 export const INVALID_INPUT_MESSAGE = 'Invalid input for interaction';
-
-// The mainnet wallet that will be used in the creation of contracts, ants and record purchases.
-export const keyfile =
-  "F:\\Keys\\ARNS3_arweave-keyfile-QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ.json";
-
-  // The Redstone Testnet wallet that will be used in the creation of contracts, ants and record purchases.
-export const testKeyfile =
-  "F:\\Keys\\ArNS2_arweave-keyfile-K6AmqvbFSiNKJkfauWt68Qg8ISwwM-3wT_8wcryuS6U.json";
+export const NON_CONTRACT_OWNER_MESSAGE = `Caller is not the owner of the ANT!`;
