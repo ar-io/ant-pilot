ad09961d623bf947ced149d6937e610e80f2f7e0
vilenarios <vilenarios@gmail.com>
2023-10-04 12:47:59 -0400

    Cleans up tooling

--- src/tools/buy-arns-name.ts
+++ src/tools/buy-arns-name.ts
@@ -1,68 +1,74 @@
-import {
-  defaultCacheOptions,
-  LoggerFactory,
-  WarpFactory,
-} from "warp-contracts";
-import * as fs from "fs";
-import { JWKInterface } from "arweave/node/lib/wallet";
-import { keyfile } from "./constants";
+import * as fs from 'fs';
 
+import { keyfile } from './constants';
+import { getContractManifest, initialize, warp } from './utilities';
+
+/* eslint-disable no-console */
 (async () => {
+  // simple setup script
+  initialize();
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~UPDATE THE BELOW~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // This is the name that will be purchased in the Arweave Name System Registry
-  const nameToBuy = "genesis";
+  const nameToBuy = process.env.ARNS_NAME ?? 'a-test-name';
 
   // This is the ANT Smartweave Contract TX ID that will be added to the registry. It must follow the ArNS ANT Specification
-  const contractTxId = "xEL3QuBjrJtlJm4DSHn7BKB5S-riLc1qCkmn3r-xkiE";
+  const contractTxId =
+    process.env.ANT_CONTRACT_TX_ID ??
+    'gh673M0Koh941OIITVXl9hKabRaYWABQUedZxW-swIA';
 
-  // This is the production ArNS Registry Smartweave Contract
-  const arnsRegistryContractTxId =
-    "bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U";
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // The lease time for purchasing the name
+  const years = 1;
 
-  // Initialize `LoggerFactory`
-  LoggerFactory.INST.logLevel("error");
-
-  // ~~ Initialize Warp ~~
-  const warp = WarpFactory.forMainnet(
-    {
-      ...defaultCacheOptions,
-      inMemory: true,
-    },
-    true
+  // load local wallet
+  const wallet = JSON.parse(
+    process.env.JWK ? process.env.JWK : fs.readFileSync(keyfile).toString(),
   );
 
-  // Get the key file used for the distribution
-  const wallet: JWKInterface = JSON.parse(
-    await fs.readFileSync(keyfile).toString()
-  );
+  // load state of contract
+  const arnsContractTxId =
+    process.env.ARNS_CONTRACT_TX_ID ??
+    'E-pRI1bokGWQBqHnbut9rsHSt9Ypbldos3bAtwg4JMc';
+
+  // get contract manifest
+  const { evaluationOptions = {} } = await getContractManifest({
+    contractTxId: arnsContractTxId,
+  });
 
   // Read the ANT Registry Contract
-  const pst = warp.pst(arnsRegistryContractTxId);
-  pst.connect(wallet);
+  const contract = warp
+    .pst(arnsContractTxId)
+    .setEvaluationOptions(evaluationOptions);
+  contract.connect(wallet);
 
   // check if this name exists in the registry, if not exit the script.
-  const currentState = await pst.currentState();
+  const currentState = await contract.readState();
   const currentStateString = JSON.stringify(currentState);
   const currentStateJSON = JSON.parse(currentStateString);
   if (currentStateJSON.records[nameToBuy] !== undefined) {
     console.log(
-      "This name %s is already taken and is not available for purchase.  Exiting.",
-      nameToBuy
+      'This name %s is already taken and is not available for purchase.  Exiting.',
+      nameToBuy,
     );
     return;
   }
 
   // Buy the available record in ArNS Registry
   console.log(
-    "Buying the record, %s using the ANT %s",
+    'Buying the record, %s using the ANT %s',
     nameToBuy,
-    contractTxId
-  );
-  const recordTxId = await pst.writeInteraction({
-    function: "buyRecord",
-    name: nameToBuy,
     contractTxId,
-  });
-  console.log("Finished purchasing the record: %s", recordTxId);
+  );
+  const recordTxId = await contract.writeInteraction(
+    {
+      function: 'buyRecord',
+      name: nameToBuy,
+      contractTxId,
+      years,
+    },
+    {
+      disableBundling: true,
+    },
+  );
+  console.log('Finished purchasing the record: %s', recordTxId);
 })();
---
+++ src/tools/evolve-contract.ts
@@ -0,0 +1,62 @@
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import * as fs from 'fs';
+import path from 'path';
+import { LoggerFactory } from 'warp-contracts';
+import { DeployPlugin } from 'warp-contracts-plugin-deploy';
+
+import { keyfile } from './constants';
+import { initialize, warp } from './utilities';
+
+/* eslint-disable no-console */
+(async () => {
+  // simple setup script
+  initialize();
+
+  // override log settings
+  LoggerFactory.INST.logLevel('none');
+
+  // load local wallet
+  const wallet: JWKInterface = JSON.parse(
+    process.env.JWK ? process.env.JWK : fs.readFileSync(keyfile).toString(),
+  );
+
+  // load state of contract
+  const arnsContractTxId =
+    process.env.ARNS_CONTRACT_TX_ID ??
+    'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U';
+
+  // ~~ Initialize SmartWeave ~~
+  const warpWithDeploy = warp.use(new DeployPlugin());
+
+  // Read the ArNS Registry Contract
+  const contract = warpWithDeploy.pst(arnsContractTxId);
+  contract.connect(wallet);
+
+  // ~~ Read contract source and initial state files ~~
+  const newLocalSourceCodeJS = fs.readFileSync(
+    path.join(__dirname, '../dist/contract.js'),
+    'utf8',
+  );
+
+  // Create the evolved source code tx
+  const evolveSrcTx = await warpWithDeploy.createSource(
+    { src: newLocalSourceCodeJS },
+    wallet,
+    true,
+  );
+  const evolveSrcTxId = await warpWithDeploy.saveSource(evolveSrcTx, true);
+  if (evolveSrcTxId === null) {
+    return 0;
+  }
+
+  // eslint-disable-next-line
+  const evolveInteractionTXId = await contract.writeInteraction(
+    { function: 'evolve', value: evolveSrcTxId },
+    {
+      disableBundling: true,
+    },
+  );
+
+  // DO NOT CHANGE THIS - it's used by github actions
+  console.log(evolveSrcTxId);
+})();
