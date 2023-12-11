ff089c06978d6d1ac0366610c998391461bca7bc
dtfiedler <dylan@ar.io>
2023-12-11 08:41:37 -0800

    chore(formatting): prettier

--- jest.config.js
+++ jest.config.js
@@ -30,4 +30,3 @@ module.exports = {
     '^.+\\.(ts|js)$': 'ts-jest',
   },
 };
-
--- src/actions/write/evolve.test.ts
+++ src/actions/write/evolve.test.ts
@@ -1,45 +1,47 @@
-import { ANTState } from "../../types";
-import { evolve } from "./evolve";
-import { INVALID_INPUT_MESSAGE } from "../../constants";
+import { INVALID_INPUT_MESSAGE } from '../../constants';
+import { ANTState } from '../../types';
+import { evolve } from './evolve';
 
 const baselineAntState: ANTState = {
-    owner: 'test',
-    evolve: 'test',
-    controllers: ['test'],
-    balances: {
-        test: 1
-    },
-    name: 'test',
-    records: {},
-    ticker: 'ANT-TEST'
-}
+  owner: 'test',
+  evolve: 'test',
+  controllers: ['test'],
+  balances: {
+    test: 1,
+  },
+  name: 'test',
+  records: {},
+  ticker: 'ANT-TEST',
+};
 
 describe('evolve', () => {
-    it.each([
-        {
-            value: 'test'
-        },
-        {
-            value: 1
-        },
-        {
-            value: true
-        },
-        {
-            value: undefined
-        }, 
-        {
-            random: 'test'
-        }
-    ])('should throw an error on bad input', async (badInput: { value: any }) => {
-        const error: any = await evolve(baselineAntState, {
-            caller: 'test',
-            input: {
-                function: 'evolve',
-                ...badInput
-            }
-        }).catch(e => e);
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toEqual(expect.stringContaining(INVALID_INPUT_MESSAGE))
-    });
-})
+  it.each([
+    {
+      value: 'test',
+    },
+    {
+      value: 1,
+    },
+    {
+      value: true,
+    },
+    {
+      value: undefined,
+    },
+    {
+      random: 'test',
+    },
+  ])('should throw an error on bad input', async (badInput: { value: any }) => {
+    const error: any = await evolve(baselineAntState, {
+      caller: 'test',
+      input: {
+        function: 'evolve',
+        ...badInput,
+      },
+    }).catch((e) => e);
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toEqual(
+      expect.stringContaining(INVALID_INPUT_MESSAGE),
+    );
+  });
+});
